92120a1d4b5517b307f27b95e48cb0028f75647e
Rustam Sayfutdinov <rstm.sf@gmail.com>
2019-03-18 17:55:19 +0300

    Выделен класс, создающий файл

---
+++ CreateDocxFromDotx/CreateDocx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace CreateDocxFromDotx
+{
+    public class CreateDocx
+    {
+        public CreateDocx()
+        {
+            try
+            {
+                File.Copy(_sourceFile, _destinationFile, true);
+                using (var document = WordprocessingDocument.Open(_destinationFile, true))
+                {
+                    document.ChangeDocumentType(WordprocessingDocumentType.Document);
+
+                    var tableMem = Utility.CreateTestDataTable("TestTable");
+                    var docTable = document.MainDocumentPart.Document.Body.Elements<Table>().FirstOrDefault();
+
+                    if (docTable != null)
+                    {
+                        var rows = docTable.Descendants<TableRow>().ToList();
+                        var myRow = (TableRow)rows.Last().Clone();
+                        rows.Last().Remove();
+
+                        for (var i = 0; i < tableMem.Rows.Count; ++i)
+                        {
+                            var rowDoc = (TableRow)myRow.Clone();
+                            var cellsDoc = rowDoc.Descendants<TableCell>().ToList();
+                            for (var j = 0; j < tableMem.Columns.Count; ++j)
+                                cellsDoc[j].Descendants<Text>().FirstOrDefault().Text = tableMem.Rows[i][j].ToString();
+
+                            docTable.Descendants<TableRow>().Last().InsertAfterSelf(rowDoc);
+                        }
+                    }
+
+                    document.Save();
+                    Console.WriteLine("Document generated at " + _destinationFile);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+
+        private static readonly string SampleFolder = Path.Combine(Environment.CurrentDirectory, "Sample");
+        private readonly string _destinationFile = Path.Combine(SampleFolder, "Doc.docx");
+        private readonly string _sourceFile = Path.Combine(SampleFolder, "TemplateDoc.dotx");
+    }
+}
--- CreateDocxFromDotx/Program.cs
+++ CreateDocxFromDotx/Program.cs
@@ -1,54 +1,18 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
-using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace CreateDocxFromDotx
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var destinationFile = Path.Combine(
-                Environment.CurrentDirectory, "Sample\\Doc.docx");
-            var sourceFile = Path.Combine(
-                Environment.CurrentDirectory, "Sample\\TemplateDoc.dotx");
             try
             {
-                File.Copy(sourceFile, destinationFile, true);
-                using (var document = WordprocessingDocument.Open(destinationFile, true))
-                {
-                    document.ChangeDocumentType(WordprocessingDocumentType.Document);
-
-                    var tableMem = Utility.CreateTestDataTable("TestTable");
-                    var docTable = document.MainDocumentPart.Document.Body.Elements<Table>().FirstOrDefault();
-
-                    if (docTable != null)
-                    {
-                        var rows = docTable.Descendants<TableRow>().ToList();
-                        var myRow = (TableRow)rows.Last().Clone();
-                        rows.Last().Remove();
-
-                        for (var i = 0; i < tableMem.Rows.Count; ++i)
-                        {
-                            var rowDoc = (TableRow)myRow.Clone();
-                            var cellsDoc = rowDoc.Descendants<TableCell>().ToList();
-                            for (var j = 0; j < tableMem.Columns.Count; ++j)
-                                cellsDoc[j].Descendants<Text>().FirstOrDefault().Text = tableMem.Rows[i][j].ToString();
-
-                            docTable.Descendants<TableRow>().Last().InsertAfterSelf(rowDoc);
-                        }
-                    }
-
-                    document.Save();
-                    Console.WriteLine("Document generated at " + destinationFile);
-                }
+                var createDocx = new CreateDocx();
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine(e.Message);
             }
             finally
             {

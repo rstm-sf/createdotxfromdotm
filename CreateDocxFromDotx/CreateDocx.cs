c09d64c09c2f9a3d9c43bf272920f980e8b8f58f
Rustam Sayfutdinov <rstm.sf@gmail.com>
2019-03-20 18:37:27 +0300

    Добавлено вставка изображения

--- CreateDocxFromDotx/CreateDocx.cs
+++ CreateDocxFromDotx/CreateDocx.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
+using A = DocumentFormat.OpenXml.Drawing;
+using DW = DocumentFormat.OpenXml.Drawing.Wordprocessing;
+using PIC = DocumentFormat.OpenXml.Drawing.Pictures;
 
 namespace CreateDocxFromDotx
 {
@@ -28,6 +31,8 @@ namespace CreateDocxFromDotx
                     dataTable = Utility.CreateTestDataTableWith5Column("TestTableWith5");
                     InsertSimpleTableWithAddColumn(dataTable, docTables[1]);
 
+                    InsertAPicture(document, Path.Combine(SampleFolder, "picture1.jpg"));
+
                     document.Save();
                     Console.WriteLine("Document generated at " + _destinationFile);
                 }
@@ -41,6 +46,108 @@ namespace CreateDocxFromDotx
         public void Open()
         {
             Process.Start(_destinationFile);
+        }
+
+        public static void InsertAPicture(WordprocessingDocument document, string fileNamePic)
+        {
+            var mainPart = document.MainDocumentPart;
+            var imagePart = mainPart.AddImagePart(ImagePartType.Jpeg);
+
+            using (var stream = new FileStream(fileNamePic, FileMode.Open))
+            {
+                imagePart.FeedData(stream);
+            }
+
+            AddImageToBody(document, mainPart.GetIdOfPart(imagePart));
+        }
+
+        private static void AddImageToBody(WordprocessingDocument wordDoc, string relationshipId)
+        {
+            var pictureProperties = new PIC.NonVisualPictureProperties(
+                new PIC.NonVisualDrawingProperties()
+                {
+                    Id = (UInt32Value) 0U,
+                    Name = "New Bitmap Image.jpg"
+                },
+                new PIC.NonVisualPictureDrawingProperties()
+            );
+
+            var picBlipFill = new PIC.BlipFill(
+                new A.Blip(
+                    new A.BlipExtensionList(
+                        new A.BlipExtension()
+                        {
+                            Uri ="{28A0092B-C50C-407E-A947-70E740481C1C}"
+                        }))
+                {
+                    Embed = relationshipId,
+                    CompressionState =
+                        A.BlipCompressionValues.Print
+                },
+                new A.Stretch(new A.FillRectangle())
+            );
+
+            var picShapeProperties = new PIC.ShapeProperties(
+                new A.Transform2D(
+                    new A.Offset()
+                    {
+                        X = 0L, Y = 0L
+                    },
+                    new A.Extents()
+                    {
+                        Cx = 990000L, Cy = 792000L
+                    }),
+                new A.PresetGeometry(new A.AdjustValueList())
+                {
+                    Preset = A.ShapeTypeValues.Rectangle
+                }
+            );
+
+            var aGraphicData = new A.GraphicData(
+                new PIC.Picture(
+                    pictureProperties,
+                    picBlipFill,
+                    picShapeProperties))
+            {
+                Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture"
+            };
+
+            var dwInline = new DW.Inline(
+                new DW.Extent()
+                {
+                    Cx = 990000L, Cy = 792000L
+                },
+                new DW.EffectExtent()
+                {
+                    LeftEdge = 0L,
+                    TopEdge = 0L,
+                    RightEdge = 0L,
+                    BottomEdge = 0L
+                },
+                new DW.DocProperties()
+                {
+                    Id = (UInt32Value) 1U,
+                    Name = "Picture 1"
+                },
+                new DW.NonVisualGraphicFrameDrawingProperties(
+                    new A.GraphicFrameLocks()
+                    {
+                        NoChangeAspect = true
+                    }),
+                new A.Graphic(aGraphicData))
+            {
+                DistanceFromTop = 0U,
+                DistanceFromBottom = 0U,
+                DistanceFromLeft = 0U,
+                DistanceFromRight = 0U,
+                EditId = "50D07946"
+            };
+
+            // Define the reference of the image.
+            var element = new Drawing(dwInline);
+
+            // Append the reference to body, the element should be in a Run.
+            wordDoc.MainDocumentPart.Document.Body.AppendChild(new Paragraph(new Run(element)));
         }
 
         private static void InsertSimpleTable(DataTable dataTable, OpenXmlElement docTable)
--- CreateDocxFromDotx/Program.cs
+++ CreateDocxFromDotx/Program.cs
@@ -4,7 +4,7 @@ namespace CreateDocxFromDotx
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             try
             {
